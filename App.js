adc9b109e76be63401616400b7d07489a28dd483
Justin Wu <jqwu@umich.edu>
2024-12-01 16:07:32 -0500

    added redux

---
+++ App.js
@@ -0,0 +1,21 @@
+// App.js
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { increment, decrement, selectCount } from './counterSlice';
+import { View, Text, Button } from 'react-native';
+
+const App = () => {
+  const count = useSelector(selectCount);
+  const dispatch = useDispatch();
+
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Redux Example</Text>
+      <Text>Count: {count}</Text>
+      <Button title="Increment" onPress={() => dispatch(increment())} />
+      <Button title="Decrement" onPress={() => dispatch(decrement())} />
+    </View>
+  );
+};
+
+export default App;
--- index.js
+++ index.js
@@ -1,9 +1,14 @@
-/**
- * @format
- */
-
-import {AppRegistry} from 'react-native';
+// index.js
+import { AppRegistry } from 'react-native';
 import App from './App';
-import {name as appName} from './app.json';
+import { name as appName } from './app.json';
+import { store } from './store';
+import { Provider } from 'react-redux';
 
-AppRegistry.registerComponent(appName, () => App);
+const ReduxExample = () => (
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
+
+AppRegistry.registerComponent(appName, () => ReduxExample);
